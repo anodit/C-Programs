6b63451f300683fbe27e40e80ebe5f3f73fad7df
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-10-05 21:14:53 +0530

    comment added

--- recursion1.cpp
+++ recursion1.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+// This function first call itself again and again until the condition is not false ,
+// afterthat it will go back to its last recursive call and then print the value..
 void fun2(int n)
 {
     if(n>0)
